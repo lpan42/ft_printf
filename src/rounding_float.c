8f84c5f7f8cc5b7ee1f10cfeb8cc493f2cf992da
Le PAN <lpan@e2r6p8.42.fr>
2019-03-02 17:57:00 +0100

    norm roundingfloat

--- src/rounding_float.c
+++ src/rounding_float.c
@@ -10,134 +10,158 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "ft_printf.h"
 #include "libft.h"
 
-char	*check_9(int count_digit, char *str, int precis)
+char	*check_9_add_onedigit(int count_digit, char *str, int precis)
 {
-	char *temp;
-	int i;
+	char	*temp;
+	int		i;
 
 	i = 0;
 	temp = NULL;
-	if(str[0] == '9')
+	while (i < count_digit + precis)
 	{
-		while(i < count_digit + precis)
-		{
-			if(str[i] == '9')
-				i++;
-			else if (str[i] == '.')
-				i++;
-			else
-				break;
-		}
-		if(str[i] != '9')
-		 	i--;
-		if(i == count_digit + precis)
-		{
-			if(!(temp = ft_strnew((count_digit + precis + 1 + 1) + 1)))
-				return(NULL);
-			ft_memset(temp, '0', count_digit + precis + 2);
-			temp[0] = '1';
-			temp[count_digit + 1] = '.';
-		}
+		if (str[i] == '9')
+			i++;
+		else if (str[i] == '.')
+			i++;
+		else
+			break ;
 	}
-	else
+	if (str[i] != '9')
+		i--;
+	if (i == count_digit + precis)
 	{
-		i = 1;
-		while(i < count_digit + precis)
-		{
-			if(str[i] == '9')
-				i++;
-			else if (str[i] == '.')
-				i++;
-			else
-				break;
-		}
-		if(str[i] != '9')
-		 	i--;
-		if(i == count_digit + precis)
-		{
-			if(!(temp = ft_strnew((count_digit + precis + 1) + 1)))
-				return(NULL);
-			ft_memset(temp, '0', count_digit + precis + 1);
-			temp[0] = str[0] + 1;
-			temp[count_digit] = '.';
-		}
+		if (!(temp = ft_strnew((count_digit + precis + 1 + 1) + 1)))
+			return (NULL);
+		ft_memset(temp, '0', count_digit + precis + 2);
+		temp[0] = '1';
+		temp[count_digit + 1] = '.';
 	}
 	return (temp);
 }
 
-char *rounding_nbr(char *str, int precis)
+char	*check_9_norm(int count_digit, char *str, int precis)
 {
-	int i;
-	char *temp;
-	int count_digit;
+	char	*temp;
+	int		i;
+
+	temp = NULL;
+	i = 1;
+	while (i < count_digit + precis)
+	{
+		if (str[i] == '9')
+			i++;
+		else if (str[i] == '.')
+			i++;
+		else
+			break ;
+	}
+	if (str[i] != '9')
+		i--;
+	if (i == count_digit + precis)
+	{
+		if (!(temp = ft_strnew((count_digit + precis + 1) + 1)))
+			return (NULL);
+		ft_memset(temp, '0', count_digit + precis + 1);
+		temp[0] = str[0] + 1;
+		temp[count_digit] = '.';
+	}
+	return (temp);
+}
+
+char	*check_9(int count_digit, char *str, int precis)
+{
+	char	*temp;
+	int		i;
+
+	i = 0;
+	temp = NULL;
+	if (str[0] == '9')
+		temp = check_9_add_onedigit(count_digit, str, precis);
+	else
+		temp = check_9_norm(count_digit, str, precis);
+	return (temp);
+}
+
+char	*bigger_than_4(int i, char *temp, int precis, int count_digit)
+{
+	if (temp[precis + count_digit] != '9')
+		temp[precis + count_digit] += 1;
+	else if (temp[precis + count_digit] == '9')
+	{
+		while (temp[i] == '9')
+		{
+			temp[i] = '0';
+			i--;
+		}
+		if (temp[i] == '.')
+			i--;
+		temp[i] += 1;
+	}
+	return (temp);
+}
+
+char	*rounding_nbr(char *str, int precis)
+{
+	int		i;
+	char	*temp;
+	int		count_digit;
 
 	temp = NULL;
 	count_digit = 0;
-	while(str[count_digit] != '.')
+	while (str[count_digit] != '.')
 		count_digit++;
 	temp = check_9(count_digit, str, precis);
-	if(temp)
+	if (temp)
 	{
 		ft_strdel(&str);
 		return (temp);
 	}
 	temp = str;
 	i = precis + count_digit;
-	if(temp[precis + count_digit + 1] > '4')
-	{
-		if(temp[precis + count_digit] != '9')
-			temp[precis + count_digit] += 1;
-		else if(temp[precis + count_digit] == '9')
-		{
-			while(temp[i] == '9')
-			{
-				temp[i] = '0';
-				i--;
-			}
-			if(temp[i] == '.')
-				i--;
-			temp[i] += 1;
-		}
-	}
-		if(!(str = ft_strndup(temp, precis + count_digit + 1)))
-			return (0);
-		ft_strdel(&temp);
-		return (str);
+	if (temp[precis + count_digit + 1] > '4')
+		bigger_than_4(i, temp, precis, count_digit);
+	if (!(str = ft_strndup(temp, precis + count_digit + 1)))
+		return (0);
+	ft_strdel(&temp);
+	return (str);
 }
 
-char *rounding_nbr_excp(char *str)
+char	*bigger_than_4_excp(int i, char *temp, int count_digit)
 {
-	int i;
-	char *temp;
-	int count_digit;
+	if (temp[count_digit + 1] != '9')
+		temp[count_digit - 1] += 1;
+	else if (temp[count_digit + 1] == '9')
+	{
+		while (temp[i] == '9')
+		{
+			temp[i] = '0';
+			i--;
+		}
+		if (temp[i] == '.')
+			i--;
+		temp[i] += 1;
+	}
+	return (temp);
+}
+
+char	*rounding_nbr_excp(char *str)
+{
+	int		i;
+	char	*temp;
+	int		count_digit;
 
 	temp = str;
 	count_digit = 0;
-	while(str[count_digit] != '.')
+	while (str[count_digit] != '.')
 		count_digit++;
 	i = count_digit;
 	if (temp[count_digit + 1] > '4')
-	{
-		if(temp[count_digit + 1] != '9')
-			temp[count_digit - 1] += 1;
-		else if (temp[count_digit + 1] == '9')
-		{
-			while(temp[i] == '9')
-			{
-				temp[i] = '0';
-				i--;
-			}
-			if(temp[i] == '.')
-				i--;
-			temp[i] += 1;
-		}
-	}
-		if(!(str = ft_strndup(temp, count_digit)))
-			return (0);
-		ft_strdel(&temp);
-		return (str);
+		bigger_than_4_excp(i, temp, count_digit);
+	if (!(str = ft_strndup(temp, count_digit)))
+		return (0);
+	ft_strdel(&temp);
+	return (str);
 }
